4e37ada9f98dc6d1d355520a0afba46aad7fd7d4
reiskaren <kreisdemenezes@gmail.com>
2024-07-18 18:18:33 -0300

    Descrição das suas alterações

--- CodeTechLibrary/src/components/Menu.jsx
+++ CodeTechLibrary/src/components/Menu.jsx
@@ -3,7 +3,7 @@ import Container from "react-bootstrap/Container"
 import Form from "react-bootstrap/Form"
 import Nav from "react-bootstrap/Nav"
 import Navbar from "react-bootstrap/Navbar"
-import NavDropdown from "react-bootstrap/NavDropdown"
+
 import logo from "../assets/logo_2.png"
 function Menu() {
   return (
