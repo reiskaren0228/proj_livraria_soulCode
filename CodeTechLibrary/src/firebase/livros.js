243c1063d5cd8970198ec8f2bc70ac970f3a10d9
Andreina Lima <andreinaadelima14@gmail.com>
2024-07-19 16:13:51 -0300

    Adicionado livros.js no firebase

---
+++ CodeTechLibrary/src/firebase/livros.js
@@ -0,0 +1,33 @@
+import { db } from "./firebaseConfig"
+import {
+  collection,
+  addDoc,
+  getDocs,
+  updateDoc,
+  deleteDoc,
+  doc,
+} from "firebase/firestore"
+
+const livrosCollection = collection(db, "livros")
+
+const createLivro = async (livro) => {
+  await addDoc(livrosCollection, livro)
+}
+
+const readLivros = async () => {
+  const snapshot = await getDocs(livrosCollection)
+  const livrosList = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+  return livrosList
+}
+
+const updateLivro = async (id, updatedLivro) => {
+  const livroDoc = doc(db, "livros", id)
+  await updateDoc(livroDoc, updatedLivro)
+}
+
+const deleteLivro = async (id) => {
+  const livroDoc = doc(db, "livros", id)
+  await deleteDoc(livroDoc)
+}
+
+export { createLivro, readLivros, updateLivro, deleteLivro }
