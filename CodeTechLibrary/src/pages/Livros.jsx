5aada72fdfffe316528a556d7c23ee647bd23194
Andreina Lima <andreinaadelima14@gmail.com>
2024-07-19 23:11:04 -0300

    Adicionado rodape ao fim da página

--- CodeTechLibrary/src/pages/Livros.jsx
+++ CodeTechLibrary/src/pages/Livros.jsx
@@ -30,32 +30,36 @@ const Livros = ({ livros }) => {
   }, [])
 
   return (
-    <div>
-      <h1>Lista de Livros</h1>
-      <div className="lista-livros">
-        {livros.map((livro) => (
-          <div key={livro.id} className="card-livro">
-            <img src={livro.imagem} alt={`Capa do livro ${livro.titulo}`} />
-            <h2 className="titulo-card">{livro.titulo}</h2>
-            <p className="livro-info">
-              <strong>Autor:</strong> {livro.autor}
-            </p>
-            <p className="livro-info">
-              <strong>Publicado em:</strong> {livro.data_da_publicacao}
-            </p>
-            <p className="livro-info">
-              <strong>Editora:</strong> {livro.editora}
-            </p>
-            <p className="livro-descricao">
-              <strong>Descrição: </strong>
-              {livro.descricao}
-            </p>
-            <Button onClick={() => handleEditar(livro.id)}>Editar</Button>
-            <Button onClick={() => handleDeleteLivro(livro.id)}>Deletar</Button>
-          </div>
-        ))}
+    <main>
+      <div>
+        <h1>Lista de Livros</h1>
+        <div className="lista-livros">
+          {livros.map((livro) => (
+            <div key={livro.id} className="card-livro">
+              <img src={livro.imagem} alt={`Capa do livro ${livro.titulo}`} />
+              <h2 className="titulo-card">{livro.titulo}</h2>
+              <p className="livro-info">
+                <strong>Autor:</strong> {livro.autor}
+              </p>
+              <p className="livro-info">
+                <strong>Publicado em:</strong> {livro.data_da_publicacao}
+              </p>
+              <p className="livro-info">
+                <strong>Editora:</strong> {livro.editora}
+              </p>
+              <p className="livro-descricao">
+                <strong>Descrição: </strong>
+                {livro.descricao}
+              </p>
+              <Button onClick={() => handleEditar(livro.id)}>Editar</Button>
+              <Button onClick={() => handleDeleteLivro(livro.id)}>
+                Deletar
+              </Button>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </main>
   )
 }
 
